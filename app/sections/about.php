ab803db04b93260a2f01e396f93b61c241b5b698
Bùi Thanh Xuân <xuanbuithanh221@gmail.com>
2023-02-17 14:02:07 +0700

    update

--- app/sections/about.php
+++ app/sections/about.php
@@ -1,1 +1,18 @@
 <?php ?>
+<div class="custom-header">
+    <span class="header-text">
+        <span class="first-letter">A</span>bout Me
+    </span>
+</div>
+
+<div class="page-content-wrapper">
+    <article class="page-content">
+        <h2>Hello! I'm Ken Longbottom.</h2>
+        <p>
+            <?php 
+                 
+            ?>
+            Full-Stack Web Developer from Hà Nội, Việt Nam. I'm a passionate and creative web developer with 1+ years of experience in web development. I have a strong passion for web development and I'm always looking for new challenges. I'm a fast learner and I'm always looking for new opportunities to improve my skills.
+        </p>
+    </article>
+</div>
