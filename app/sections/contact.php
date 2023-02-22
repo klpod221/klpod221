45f7c284d70f042a0294f2976b77688357a4dc65
Bùi Thanh Xuân <xuanbuithanh221@gmail.com>
2023-02-22 09:13:01 +0700

    update

--- app/sections/contact.php
+++ app/sections/contact.php
@@ -4,7 +4,7 @@
         <span class="first-letter">G</span>et In Touch
     </span>
 </div>
-<div class="page-content-wrapper">
+<div class="page-content-wrapper with-pd">
     <article class="page-content page-contact">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d476859.2078445619!2d105.37249178167059!3d20.974087357160805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135008e13800a29%3A0x2987e416210b90d!2sHanoi%2C%20Vietnam!5e0!3m2!1sen!2s!4v1676626538233!5m2!1sen!2s" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
@@ -38,7 +38,7 @@
         <span class="first-letter">C</span>ontact Form
     </span>
 </div>
-<div class="page-content-wrapper">
+<div class="page-content-wrapper with-pd">
     <article class="page-content page-contact">
         <form class="contact-form" id="contact-form">
             <div class="input-grid">
